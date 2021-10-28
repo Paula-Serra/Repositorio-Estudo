f991ac75803f60d1f1612ee87d6d15a423f3f48c
Paula-Serra <93009200+Paula-Serra@users.noreply.github.com>
2021-10-27 23:36:29 -0300

    HighscoreText
    Highscore do game funcionando

---
+++ Highscore.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class jogador : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float forcaPulo = 700;
+
+    public LayerMask layerChao;
+
+    public float distanciaMinimaChao = 1;
+
+    private bool estaNoChao;
+
+    private float pontos;
+
+    private float highscore;
+
+    public float multiplicadorPontos = 1;
+
+    public Text pontosText;
+
+    public Text highscoreText;
+
+    public Animator animatorComponent;
+
+    private void Start()
+    {
+        highscore = PlayerPrefs.GetFloat("HIGHSCORE");
+
+        highscoreText.text = $"Highscore: {Mathf.FloorToInt(highscore)}";
+    }
+
+           void Update()
+    {
+        pontos += Time.deltaTime * multiplicadorPontos;
+
+        pontosText.text = $"Pontos: {Mathf.FloorToInt(pontos)}";
+
+       if (Input.GetKeyDown(KeyCode.UpArrow))
+       {
+           Pular();
+       }
+        if ( Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Abaixar();
+        }
+       else if ( Input.GetKeyUp(KeyCode.DownArrow))
+       {
+           Levantar();
+       }
+    }
+
+        void Pular()
+    {
+        if (estaNoChao)
+        {
+            rb.AddForce(Vector2.up * forcaPulo);
+        }
+    }
+
+    void Abaixar()
+    {
+        animatorComponent.SetBool("Abaixado", true);
+    }
+    void Levantar()
+    {
+        animatorComponent.SetBool("Abaixado", false);
+    }
+
+     private void FixedUpdate()
+     {
+        estaNoChao = Physics2D.Raycast(transform.position, Vector2.down, distanciaMinimaChao, layerChao);
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Inimigo"))
+        {
+            if(pontos > highscore)
+            {
+                highscore = pontos;
+
+                PlayerPrefs.SetFloat("HIGHSCORE",highscore);
+            }
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
