7229a7793115ffe1658da5cb455db86349601329
Paula-Serra <93009200+Paula-Serra@users.noreply.github.com>
2021-10-30 15:13:29 -0300

    Jogo
    Velocidade , time e botão reiniciar.

---
+++ Jogo.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Jogo : MonoBehaviour
+{
+    public float modificadorVelocidade = 1f;
+
+    public float velocidade = 4.5f;
+
+    public float velocidadeMaxima = 10f;
+
+    private void Update()
+    {
+        velocidade = Mathf.Clamp(
+            velocidade + modificadorVelocidade * Time.deltaTime,
+            0,
+            velocidadeMaxima
+            );
+    }
+    public void ReiniciarJogo()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        Time.timeScale = 1;
+    }
+}
